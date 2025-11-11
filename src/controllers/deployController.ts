5bce7b5c2278773d200266c4a38080038c18ab95
Sunilkumar <sunilcse2006@gmail.com>
2025-11-11 22:21:24 +0530

    second commit

--- src/controllers/deployController.ts
+++ src/controllers/deployController.ts
@@ -17,24 +17,29 @@ publisher.on("error", err => console.error("❌ Redis error:", err));
 const git = simpleGit();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+// console.log(__dirname);
 
 export const deployService = async function (req: Request, res: Response) {
     const {repoUrl} = req.body;
     const id = uuid();
-   await git.clone(repoUrl, path.join(__dirname,`output/${id}`));
-   const files = getAllFiles(path.join(__dirname,`output/${id}`));
+   await git.clone(repoUrl, path.join(__dirname,`../../output/${id}`));
+   const files = getAllFiles(path.join(__dirname,`../../output/${id}`));
+   const rootDir = path.join(__dirname, "../../");
+   const outputDir = path.join(rootDir, "output");
    try {
+  const currectPath = path.join(__dirname, "../../output");
   for (const file of files) {
     if (file.includes(".git") || file.includes(".DS_Store")) continue;
     console.log("📤 Uploading:", file);
-    await uploadFile(file.slice(__dirname.length + 1), file);
+    console.log(__dirname);
+    await uploadFile("output/" + file.slice(outputDir.length + 1), file);
   }
 
   console.log("✅ Upload done, pushing status to Redis...", id);
   await publisher.hSet("status", id, "uploading");
   await publisher.lPush("build-queue", id);
 
-  res.json({ id });
+  res.json({ id});
 } catch (err) {
   console.error("❌ Error during deployService:", err);
   res.status(500).json({ error : "Internal Server Error" });
